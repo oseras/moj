42e2a0e975ff7cf23b865b8fbfe6ffa26c2a87d6
oseras <16389891+oseras@users.noreply.github.com>
2025-07-29 14:53:38 +0300

    Create .js

---
+++ .js
@@ -0,0 +1,29 @@
+{
+  "headline": "🧿 Oseras IPTV Menü",
+  "menu": [
+    {
+      "type": "separator",
+      "label": "📺 Ana Liste"
+    },
+    {
+      "icon": "movie",
+      "label": "Tüm Kanallar (Panel + GitHub)",
+      "extensionLabel": "Güncel",
+      "data": "playlist:https://oseras-ip.m3u.workers.dev"
+    },
+    {
+      "type": "separator",
+      "label": "🎯 Özel İşlemler"
+    },
+    {
+      "icon": "refresh",
+      "label": "Yeniden Yükle",
+      "action": "reload"
+    },
+    {
+      "icon": "exit-to-app",
+      "label": "Çık",
+      "action": "back"
+    }
+  ]
+}
